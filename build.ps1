5c1316ae8c1e6b413afc7e179aa4d3a9f0c8002f
Gary Ewan Park <gep13@gep13.co.uk>
2019-03-04 13:46:41 -0800

    (GH-1) Initial implementation

---
+++ build.ps1
@@ -0,0 +1,33 @@
+
+
+[cmdletbinding(DefaultParameterSetName = 'task')]
+param(
+    [parameter(ParameterSetName = 'task', Position = 0)]
+    [string[]]$Task = 'default',
+
+    [parameter(ParameterSetName = 'help')]
+    [switch]$Help
+)
+
+Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+    Install-module -Name PSDepend -Repository PSGallery
+}
+Import-Module -Name PSDepend
+Invoke-PSDepend -Path .\requirements.psd1 -Install -Import -Force
+if (-not (Get-Module -Name BuildHelpers -ListAvailable)) {
+    Install-Module -Name BuildHelpers -AllowClobber
+}
+Import-Module -Name BuildHelpers
+
+if ($PSBoundParameters.ContainsKey('help')) {
+    Get-PSakeScriptTasks -buildFile "$PSScriptRoot\psake.ps1" |
+        Format-Table -Property Name, Description, Alias, DependsOn
+} else {
+    Set-BuildEnvironment -Force
+
+    Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo
+    exit ( [int]( -not $psake.build_success ) )
+}
